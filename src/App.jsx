2c9298d76a9d4476a1c11d8de9bb3d5640732a08
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-03 22:48:37 -0700

    Fixed the css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Heading.jsx'
 import Entry from './components/Entry.jsx'
 import Japan from './images/japan.png'
+import './App.css'
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <main className = "container">
         <Entry 
           src = {Japan}
-          alt = "Moun Fuji"
+          alt = "Mount Fuji"
           title = "Mount Fuji"
           country = "Japan"
           googleMapsLink = "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
